ceae529d9d26c5c81c470bf0d6e163e7b42ab21c
praneeth <gp.gunturu@gmail.com>
2026-03-05 17:55:59 -0600

    refactor AuthForm to use ControllerInput for form fields

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,11 +1,13 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
-import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
+import { Field, FieldGroup, FieldSet } from "@/components/ui/field";
 import { authFormSchema } from "@/lib/utils";
 import {useForm} from "react-hook-form"
 import {zodResolver} from "@hookform/resolvers/zod"
 import Link from "next/link";
 import z from "zod";
+import ControllerInput from "./ControllerInput";
 
 interface AuthFormProps {
   type: string;
@@ -14,56 +16,79 @@ interface AuthFormProps {
 export default function AuthForm({ type }: AuthFormProps) {
 
   const formSchema = authFormSchema(type)
+  type formValues = z.infer<typeof formSchema>
+
+  const form = useForm<formValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+    }
+  })
+
+  function onSubmit(data: formValues) {
+    console.log(data);
+    
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 space-y-12">
       <div className="w-full max-w-md border rounded-2xl p-6 shadow-sm">
-        <h2 className="text-xl font-semibold mb-4 text-center">Sign In</h2>
-        <form>
+        <h2 className="text-xl font-semibold mb-4 text-center">{type === "sign-in" ? "Sign In" : "Sign Up"}</h2>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
           <FieldSet>
             <FieldGroup>
               {type === "sign-up" && (
                 <>
                   <div className="flex gap-4">
-                    <Field>
-                      <FieldLabel>First Name</FieldLabel>
-                      <Input placeholder="Enter first name" />
-                    </Field>
-                    <Field>
-                      <FieldLabel>Last Name</FieldLabel>
-                      <Input placeholder="Enter last name" />
-                    </Field>
+                    <ControllerInput 
+                      control={form.control}
+                      name="firstName"
+                      label="First Name"
+                      placeholder="Enter first name"
+                    />
+                    <ControllerInput 
+                      control={form.control}
+                      name="lastName"
+                      label="Last Name"
+                      placeholder="Enter last name"
+                    />
                   </div>
                 </>
               )}
-              <Field>
-                <FieldLabel>Email</FieldLabel>
-                <Input placeholder="Enter email" />
-              </Field>
-
-              <Field>
-                <FieldLabel>Password</FieldLabel>
-                <Input placeholder="Enter password" type="password"/>
-              </Field>
+              <ControllerInput 
+                control={form.control}
+                name="email"
+                label="Email"
+                placeholder="Enter the email"
+              />
+              <ControllerInput 
+                control={form.control}
+                name="password"
+                label="Password"
+                placeholder="Enter the password"
+              />
             </FieldGroup>
           </FieldSet>
 
           <Field>
-            <Button type="submit" className="mt-4">
-              Sign In
+            <Button type="submit" className="mt-8">
+              {type === "sign-in" ? "Sign In" : "Sign Up"}
             </Button>
           </Field>
         </form>
 
-        <footer className="flex justify-center gap-1">
-          <p className="text-14 font-normal text-gray-600">
+        <footer className="flex justify-center gap-1 mt-2">
+          <p className="text-sm font-normal text-gray-600">
             {type === "sign-in"
               ? "Don't have an account?"
               : "Already have an account"}
           </p>
           <Link
             href={type === "sign-in" ? "/sign-up" : "/sign-in"}
-            className="text-14 cursor-pointer text-md text-gray-800"
+            className="text-sm cursor-pointer text-md text-gray-600"
           >
             {type === "sign-in" ? "Sign Up" : "Sign In"}
           </Link>
---
+++ src/components/ControllerInput.tsx
@@ -0,0 +1,42 @@
+import { authFormSchema } from "@/lib/utils";
+import { Control, Controller, FieldPath } from "react-hook-form";
+import z from "zod";
+import { Field, FieldError, FieldLabel } from "./ui/field";
+import { Input } from "./ui/input";
+
+const formSchema = authFormSchema("sign-up");
+
+type formValues = z.infer<typeof formSchema>;
+
+interface ControllerInputProps {
+  control: Control<formValues>;
+  name: FieldPath<formValues>;
+  label: string;
+  placeholder: string;
+}
+
+export default function ControllerInput({
+  control,
+  name,
+  label,
+  placeholder,
+}: ControllerInputProps) {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState }) => (
+        <Field data-invalid={fieldState.invalid}>
+          <FieldLabel htmlFor={name}>{label}</FieldLabel>
+          <Input 
+            id={name} 
+            placeholder={placeholder} 
+            {...field} 
+            type={name === 'password' ? 'password' : 'text'}
+          />
+          {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+        </Field>
+      )}
+    />
+  );
+}
