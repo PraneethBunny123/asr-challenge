3d4e83b83786b0c053d8df1550ba96ae609e3162
praneeth <gp.gunturu@gmail.com>
2026-03-22 12:34:14 -0500

    fix UI errors

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,9 +6,11 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-background">
       <Header />
-      <main className="flex-1 p-6">{children}</main>
+      <main className="flex-1">
+        <div className="mx-auto max-w-6xl px-6 py-8">{children}</div>
+      </main>
     </div>
   );
 }
